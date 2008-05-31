9aec4efefa2adec49e5943eefa98e04ecccd9d12
Paul Beckingham <paul@beckingham.net>
2008-05-30 21:35:49 -0400

    - Added proper copyright, licensing details to all source. - Eliminated redundant stlmacros.h heaer file.

--- src/task.cpp
+++ src/task.cpp
@@ -1,7 +1,27 @@
 ////////////////////////////////////////////////////////////////////////////////
+// task - a command line task list manager.
+//
 // Copyright 2006 - 2008, Paul Beckingham.
 // All rights reserved.
 //
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the
+//
+//     Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor,
+//     Boston, MA
+//     02110-1301
+//     USA
 //
 ////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
@@ -16,7 +36,6 @@
 #include "Config.h"
 #include "Date.h"
 #include "Table.h"
-#include "stlmacros.h"
 #include "TDB.h"
 #include "T.h"
 #include "task.h"
@@ -2307,6 +2326,14 @@ void handleVersion (Config& conf)
             << PACKAGE
             << " "
             << VERSION
+            << std::endl
+            << std::endl
+            << "Task comes with ABSOLUTELY NO WARRANTY; for details read the COPYING file"
+            << std::endl
+            << "included.  This is free software, and you are welcome to redistribute it"
+            << std::endl
+            << "under certain conditions; again, see the COPYING file for details."
+            << std::endl
             << std::endl
             << std::endl
             << table.render ()
--- src/task.h
+++ src/task.h
@@ -1,6 +1,27 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright 2006 - 2008, Paul Beckingham.  All rights reserved.
+// task - a command line task list manager.
 //
+// Copyright 2006 - 2008, Paul Beckingham.
+// All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation; either version 2 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the
+//
+//     Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor,
+//     Boston, MA
+//     02110-1301
+//     USA
 //
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -13,12 +34,19 @@
 #include "color.h"
 #include "TDB.h"
 #include "T.h"
-#include "stlmacros.h"
 #include "../auto.h"
 
 #ifndef max
 #define max(a,b) ((a) > (b) ? (a) : (b))
 #endif
+
+#define foreach(i, c)                                              \
+for (typeof (c) *foreach_p = & (c);                                \
+     foreach_p;                                                    \
+     foreach_p = 0)                                                \
+  for (typeof (foreach_p->begin()) i = foreach_p->begin();         \
+       i != foreach_p->end();                                      \
+       ++i)
 
 // parse.cpp
 void parse (std::vector <std::string>&, std::string&, T&);
