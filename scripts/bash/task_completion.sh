3072476e5395b22e23696667bf38c8bd84734ca0
Paul Beckingham <paul@beckingham.net>
2009-07-03 14:13:33 -0400

    Enhancement
    - Added missing (new) commands to the completion script, and removed
      the newly obsolete undelete command.

--- scripts/bash/task_completion.sh
+++ scripts/bash/task_completion.sh
@@ -64,7 +64,7 @@ _task()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    opts="active add annotate append calendar color completed delete done duplicate edit export ghistory help history import info list long ls newest next oldest overdue projects start stats stop summary tags timesheet undelete undo version"
+    opts="active add all annotate append calendar color completed delete done duplicate edit export ghistory help history import info list long ls newest next oldest overdue projects recurring shell start stats stop summary tags timesheet undo version waiting"
 
     case "${cur}" in
         pro*:*)
