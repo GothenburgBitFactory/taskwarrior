3af51944427e0ecb75a7e154573729e8cff94a8b
Owen Clarke <o.clarke@gmail.com>
2011-07-01 09:04:52 -0400

    Bug #797
    - Fixed bug #797 which corrects a build problem with 'srandom' (thanks to Owen
      Clarke).
    
    Signed-off-by: Paul Beckingham <paul@beckingham.net>

--- src/main.cpp
+++ src/main.cpp
@@ -52,9 +52,9 @@ int main (int argc, const char** argv)
   srand (time (NULL));
   #endif
 #else
-  #ifdef HAVE_SRANDOM
   struct timeval tv;
   gettimeofday (&tv, NULL);
+  #ifdef HAVE_SRANDOM
   srandom (tv.tv_usec);
   #else
   srand (tv.tv_usec);
