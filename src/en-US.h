a7f694386aeddef06429aa950ddabeb2019da206
Paul Beckingham <paul@beckingham.net>
2011-08-01 01:26:59 -0400

    Code Cleanup
    - Eliminated obsolete Variant code.

--- src/en-US.h
+++ src/en-US.h
@@ -465,9 +465,5 @@
 #define STRING_UTIL_KIBIBYTES        "KiB"
 #define STRING_UTIL_BYTES            "B"
 
-// Variant
-#define STRING_VARIANT_REL_BOOL      "Cannot perform relational comparison on Boolean types."
-#define STRING_VARIANT_REL_UNKNOWN   "Cannot perform relational comparison on unknown types."
-
 #endif
 
