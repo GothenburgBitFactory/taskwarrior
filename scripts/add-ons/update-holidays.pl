1f8e823593824acef0a7b5d6dd207045fde514a4
Paul Beckingham <paul@beckingham.net>
2012-02-21 17:23:57 -0500

    Holidays Updated
    - Modified update-holidays.pl script to include the MIT license.
    - Added the URLs used to download the data, in the data.
    - Added a refresh script to freshen the files.

--- scripts/add-ons/update-holidays.pl
+++ scripts/add-ons/update-holidays.pl
@@ -180,10 +180,35 @@ for my $holiday (split /\n/ms, $data)
 if (open my $fh, '>:utf8', $file)
 {
   print $fh
+        "###############################################################################\n",
         "# International Holiday Data provided by Holidata.net\n",
-        "#\n",
         "# ${url_current}\n",
+        "# ${url_next}\n",
         "#\n",
+        "# Copyright 2006-2012, Paul Beckingham, Federico Hernandez.\n",
+        "#\n",
+        "# Permission is hereby granted, free of charge, to any person obtaining a copy\n",
+        "# of this software and associated documentation files (the \"Software\"), to deal\n",
+        "# in the Software without restriction, including without limitation the rights\n",
+        "# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n",
+        "# copies of the Software, and to permit persons to whom the Software is\n",
+        "# furnished to do so, subject to the following conditions:\n",
+        "#\n",
+        "# The above copyright notice and this permission notice shall be included\n",
+        "# in all copies or substantial portions of the Software.\n",
+        "#\n",
+        "# THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS\n",
+        "# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n",
+        "# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL\n",
+        "# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n",
+        "# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n",
+        "# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n",
+        "# SOFTWARE.\n",
+        "#\n",
+        "# http://www.opensource.org/licenses/mit-license.php\n",
+        "#\n",
+        "###############################################################################\n",
+        "\n",
         $content;
 
   close $fh;
