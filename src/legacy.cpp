GothenburgBitFactory/taskwarrior

commit 0ac9a4b3df4b1ad21715890b20ecf7040d9d8201

Deprecated
- Made proper note of deprecated configuration variables.

src/legacy.cpp
@@ -145,9 +145,15 @@ std::string legacyCheckForDeprecatedVariables ()
         it->first.substr (it->first.length () - 6) == ".limit")
       deprecated.push_back (it->first);
 
+    // report.*.annotaitons
+    if (it->first.substr (0, 7) == "report." &&
+        it->first.substr (it->first.length () - 12) == ".annotations")
+      deprecated.push_back (it->first);
+
     if (it->first == "echo.command"      ||
         it->first == "edit.verbose"      ||
         it->first == "next"              ||
+        it->first == "annotations"       ||
         it->first == "export.ical.class")
       deprecated.push_back (it->first);
   }

src/commands/CmdShow.cpp
@@ -72,7 +72,6 @@ int CmdShow::execute (std::string& output)
   std::string recognized =
     " abbreviation.minimum"
     " active.indicator"
-    " annotations"
     " avoidlastcolumn"
     " bulk"
     " burndown.bias"

commit 9951da5710106a0b858783605b563a663a93f565

Bug
- Added bounds check before call to string::substr to avoid passing a
  negative start pos. Test rc.t was broken from commit 0ac9a4b because
  it contains 'report.:b' and legacyCheckForDeprecatedVariables was
  trying to substr starting from pos 'length() - 12' (-3).

src/legacy.cpp
@@ -141,12 +141,14 @@ std::string legacyCheckForDeprecatedVariables ()
   for (it = context.config.begin (); it != context.config.end (); ++it)
   {
     // report.*.limit
-    if (it->first.substr (0, 7) == "report." &&
+    if (it->first.length () > 13 &&
+        it->first.substr (0, 7) == "report." &&
         it->first.substr (it->first.length () - 6) == ".limit")
       deprecated.push_back (it->first);
 
     // report.*.annotaitons
-    if (it->first.substr (0, 7) == "report." &&
+    if (it->first.length () > 19 &&
+        it->first.substr (0, 7) == "report." &&
         it->first.substr (it->first.length () - 12) == ".annotations")
       deprecated.push_back (it->first);
 