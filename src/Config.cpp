132935c04e0dbed0fa96fa2c88f681e4ec6b1b49
Paul Beckingham <paul@beckingham.net>
2014-02-19 22:52:49 -0500

    Themes
    - New themes: dark-default-16.theme, dark-gray-blue-256.theme

--- src/Config.cpp
+++ src/Config.cpp
@@ -548,6 +548,8 @@ void Config::createDefaultRC (const std::string& rc, const std::string& data)
            << "#include " << TASK_RCDIR << "/dark-violets-256.theme\n"
            << "#include " << TASK_RCDIR << "/dark-yellow-green.theme\n"
            << "#include " << TASK_RCDIR << "/dark-gray-256.theme\n"
+           << "#include " << TASK_RCDIR << "/dark-default-16.theme\n"
+           << "#include " << TASK_RCDIR << "/dark-gray-blue-256.theme\n"
            << "\n";
 
   // Write out the new file.
