f8bb948b900099b1b06aab5515c3429af4c4f3f7
Paul Beckingham <paul@beckingham.net>
2014-05-25 13:18:30 -0400

    Text
    - Added formatter for <int, int, std::string>.  There cannot be many
      combinations remaining.

--- src/text.cpp
+++ src/text.cpp
@@ -976,6 +976,20 @@ const std::string format (
 const std::string format (
   const std::string& fmt,
   int arg1,
+  int arg2,
+  const std::string& arg3)
+{
+  std::string output = fmt;
+  replace_positional (output, "{1}", format (arg1));
+  replace_positional (output, "{2}", format (arg2));
+  replace_positional (output, "{3}", arg3);
+  return output;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+const std::string format (
+  const std::string& fmt,
+  int arg1,
   const std::string& arg2)
 {
   std::string output = fmt;
