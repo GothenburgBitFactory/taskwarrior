3302707768fea7e4350436f6fbcb822c9163491d
Paul Beckingham <paul@beckingham.net>
2014-05-28 23:47:14 -0400

    CmdSummary
    - Converted from OldDuration to Duration.

--- src/commands/CmdSummary.cpp
+++ src/commands/CmdSummary.cpp
@@ -31,7 +31,7 @@
 #include <Context.h>
 #include <Filter.h>
 #include <ViewText.h>
-#include <OldDuration.h>
+#include <Duration.h>
 #include <text.h>
 #include <util.h>
 #include <i18n.h>
@@ -159,7 +159,7 @@ int CmdSummary::execute (std::string& output)
 
       view.set (row, 1, countPending[i->first]);
       if (counter[i->first])
-        view.set (row, 2, OldDuration ((int) (sumEntry[i->first] / (double)counter[i->first])).format ());
+        view.set (row, 2, Duration ((int) (sumEntry[i->first] / (double)counter[i->first])).format ());
 
       int c = countCompleted[i->first];
       int p = countPending[i->first];
