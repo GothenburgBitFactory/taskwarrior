1a17aac77bb08effa9fdb74736b86d7ddc75cf25
Paul Beckingham <paul@beckingham.net>
2014-08-16 16:38:04 -0400

    TW-1377
    - TW-1377 Tags in default.command are treated as "operator text".

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -567,12 +567,15 @@ void Parser::injectDefaults ()
         // because captureFirst inserts args immediately after the command, and
         // so has the effect of reversing the list.
         std::vector <std::string> args;
-        Lexer::token_split (args, defaultCommand);
+        split (args, defaultCommand, ' ');
         std::vector <std::string>::reverse_iterator r;
         for (r = args.rbegin (); r != args.rend (); ++r)
         {
-          Tree* t = captureFirst (*r);
-          t->tag ("DEFAULT");
+          if (*r != "")
+          {
+            Tree* t = captureFirst (*r);
+            t->tag ("DEFAULT");
+          }
         }
 
         std::string combined;
