ddb53137477c7e60cda3eb88d0d3dde1c325293b
Paul Beckingham <paul@beckingham.net>
2014-08-17 14:37:37 -0400

    Parser
    - ::findStrayModifications scans all nodes.

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1606,7 +1606,7 @@ void Parser::findStrayModifications ()
       command == "log")
   {
     std::vector <Tree*> nodes;
-    collect (nodes, false);
+    collect (nodes, true);
     std::vector <Tree*>::iterator i;
     for (i = nodes.begin (); i != nodes.end (); ++i)
     {
