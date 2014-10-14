8bb17fb2e8c1396921e242afe6400a779b797c7f
Paul Beckingham <paul@beckingham.net>
2014-10-13 23:11:46 -0400

    CLI
    - CLI is a streamlined version of Parser, now that wisdom is upon us.

---
+++ src/CLI.cpp
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2006 - 2014, Paul Beckingham, Federico Hernandez.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// http://www.opensource.org/licenses/mit-license.php
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cmake.h>
+#include <Context.h>
+#include <CLI.h>
+#include <i18n.h>
+
+extern Context context;
+
+////////////////////////////////////////////////////////////////////////////////
+CLI::CLI ()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+CLI::~CLI ()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void CLI::alias (const std::string& name, const std::string& value)
+{
+  _aliases.insert (std::pair <std::string, std::string> (name, value));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void CLI::entity (const std::string& name, const std::string& value)
+{
+  _entities.insert (std::pair <std::string, std::string> (name, value));
+}
+
+////////////////////////////////////////////////////////////////////////////////
