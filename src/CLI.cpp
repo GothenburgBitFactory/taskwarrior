c54e0d6d05b3e45c6fc589f8bd7cc1d55f1c9347
Paul Beckingham <paul@beckingham.net>
2014-10-14 00:49:15 -0400

    CLI
    - Added new data to ::dump.

--- src/CLI.cpp
+++ src/CLI.cpp
@@ -247,6 +247,13 @@ void CLI::dump (const std::string& label) const
   std::map <std::string, std::string>::const_iterator m;
   for (m = _overrides.begin (); m != _overrides.end (); ++m)
     std::cout << "#  _overrides '" << m->first << "' --> '" << m->second << "'\n";
+
+  for (i = _filter.begin (); i != _filter.end (); ++i)
+    std::cout << "#   _filter '" << *i << "'\n";
+
+  for (i = _modifications.begin (); i != _modifications.end (); ++i)
+    std::cout << "#   _modifications '" << *i << "'\n";
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
