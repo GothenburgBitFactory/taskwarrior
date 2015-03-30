1e411ed4b8e04027f780f2204f7fe38c678356ff
Paul Beckingham <paul@beckingham.net>
2015-03-29 23:01:33 -0400

    Tests: Corrected lexer text '\''

--- test/lexer.t.cpp
+++ test/lexer.t.cpp
@@ -261,7 +261,7 @@ int main (int argc, char** argv)
     // String
     { "'one two'",                                    { { "'one two'",                                    Lexer::Type::string       }, NO, NO, NO, NO }, },
     { "\"three\"",                                    { { "\"three\"",                                    Lexer::Type::string       }, NO, NO, NO, NO }, },
-    { "'\\''",                                        { { "'\\''",                                        Lexer::Type::string       }, NO, NO, NO, NO }, },
+    { "'\\''",                                        { { "'''",                                          Lexer::Type::string       }, NO, NO, NO, NO }, },
     { "\"\\\"\"",                                     { { "\"\\\"\"",                                     Lexer::Type::string       }, NO, NO, NO, NO }, },
     { "\"\tfoo\t\"",                                  { { "\"\tfoo\t\"",                                  Lexer::Type::string       }, NO, NO, NO, NO }, },
     { "\"\\u20A43\"",                                 { { "\"₤3\"",                                       Lexer::Type::string       }, NO, NO, NO, NO }, },
