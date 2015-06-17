72073b7515d1c096e00cd16d2da8c1deda084182
Paul Beckingham <paul@beckingham.net>
2015-06-16 21:53:51 -0400

    CmdContext: Converted from CLI to CLI2

--- src/commands/CmdContext.cpp
+++ src/commands/CmdContext.cpp
@@ -54,7 +54,7 @@ int CmdContext::execute (std::string& output)
   std::stringstream out;
 
   // Get the non-attribute, non-fancy command line arguments.
-  std::vector <std::string> words = context.cli.getWords ();
+  std::vector <std::string> words = context.cli2.getWords ();
 
   if (words.size () > 0)
   {
