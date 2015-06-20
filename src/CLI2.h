20d259a1290db935e8fbb9906eeb2a2251b54b98
Paul Beckingham <paul@beckingham.net>
2015-06-20 11:59:37 -0700

    CLI2: Removed unused ::addArgs method

--- src/CLI2.h
+++ src/CLI2.h
@@ -124,6 +124,7 @@ private:
 
   bool isUUIDList       (const std::string&) const;
 
+  // These methods come up iwht reasons not to Lex a token. Probably no longer needed.
   bool disqualifyInsufficientTerms (const std::vector <std::pair <std::string, Lexer::Type>>&) const;
   bool disqualifyNoOps             (const std::vector <std::pair <std::string, Lexer::Type>>&) const;
   bool disqualifyOnlyParenOps      (const std::vector <std::pair <std::string, Lexer::Type>>&) const;
