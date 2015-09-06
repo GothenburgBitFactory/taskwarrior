58ec954d11eeaf2bfe7c79ef82557a23bcd4a1de
Paul Beckingham <paul@beckingham.net>
2015-09-06 02:08:36 -0400

    CmdExport: Removed redundant arg checks

--- src/commands/CmdExport.cpp
+++ src/commands/CmdExport.cpp
@@ -62,9 +62,6 @@ int CmdExport::execute (std::string& output)
   std::vector <Task> filtered;
   filter.subset (filtered);
 
-  if (filter.hasModifications ())
-    throw std::string (STRING_ERROR_NO_MODS);
-
   // Obey 'limit:N'.
   int rows = 0;
   int lines = 0;
