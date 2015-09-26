860a1de034863455c0e039279009e50517338b15
Paul Beckingham <paul@beckingham.net>
2015-09-26 15:43:07 -0400

    ISO8601d: Added ::operator-

--- src/ISO8601.h
+++ src/ISO8601.h
@@ -77,11 +77,11 @@ public:
   bool sameWeek   (const ISO8601d&) const;
   bool sameMonth  (const ISO8601d&) const;
   bool sameYear   (const ISO8601d&) const;
-
   ISO8601d operator+  (const int);
   ISO8601d operator-  (const int);
   ISO8601d& operator+= (const int);
   ISO8601d& operator-= (const int);
+  time_t operator- (const ISO8601d&);
 
 private:
   void clear ();
