c0952892087b4bfe9dfc562082ccf8eea8312548
Paul Beckingham <paul@beckingham.net>
2015-09-26 15:33:45 -0400

    ISO8601d: Added ::operator+

--- src/ISO8601.h
+++ src/ISO8601.h
@@ -78,6 +78,8 @@ public:
   bool sameMonth  (const ISO8601d&) const;
   bool sameYear   (const ISO8601d&) const;
 
+  ISO8601d operator+  (const int);
+
 private:
   void clear ();
   bool parse_date_time     (Nibbler&);
