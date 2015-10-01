97dbb0cfe4dc5b7bf09fb6a17281112db0b61329
Tomas Babej <tomasbabej@gmail.com>
2015-10-01 07:51:42 +0200

    CmdModify: Do not use filter context

--- src/commands/CmdModify.cpp
+++ src/commands/CmdModify.cpp
@@ -45,7 +45,7 @@ CmdModify::CmdModify ()
   _read_only             = false;
   _displays_id           = false;
   _needs_gc              = false;
-  _uses_context          = true;
+  _uses_context          = false;
   _accepts_filter        = true;
   _accepts_modifications = true;
   _accepts_miscellaneous = false;
