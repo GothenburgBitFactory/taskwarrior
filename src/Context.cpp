8e214d1b14603e9671bd9d4f9942065513c900df
Paul Beckingham <paul@beckingham.net>
2015-11-01 21:39:19 -0500

    Context: Converted from strtol to std::stoi

--- src/Context.cpp
+++ src/Context.cpp
@@ -611,7 +611,7 @@ void Context::getLimits (int& rows, int& lines)
     }
     else
     {
-      rows = (int) strtol (limit.c_str (), NULL, 10);
+      rows = std::stoi (limit);
       lines = 0;
     }
   }
