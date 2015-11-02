204c95993b42a895bd60a6ab8f159275dd6462fd
Paul Beckingham <paul@beckingham.net>
2015-11-02 18:40:49 -0500

    Revert "CLI2: Converted from strtol to std::stoi"
    This reverts commit 91afa9b5677babe7d731de63b7d9eea93fb9b1de.

--- src/CLI2.cpp
+++ src/CLI2.cpp
@@ -1772,8 +1772,8 @@ void CLI2::insertIDExpr ()
           else
           {
             bool ascending = true;
-            int low  = std::stoi (r->first);
-            int high = std::stoi (r->second);
+            int low  = strtol (r->first.c_str (),  NULL, 10);
+            int high = strtol (r->second.c_str (), NULL, 10);
             if (low <= high)
               ascending = true;
             else
