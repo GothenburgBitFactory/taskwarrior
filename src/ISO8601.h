aafb33287d7751877c1ab42c2d13ea9c03b99098
Paul Beckingham <paul@beckingham.net>
2015-12-23 08:09:40 -0500

    TW-1733: taskwarrior 2.5.0 can not compile FreeBSD 10.1
    - Thanks to ribbon.

--- src/ISO8601.h
+++ src/ISO8601.h
@@ -97,7 +97,6 @@ public:
   bool sameWeek   (const ISO8601d&) const;
   bool sameMonth  (const ISO8601d&) const;
   bool sameYear   (const ISO8601d&) const;
-  ISO8601d operator+  (time_t);
   ISO8601d operator+  (const int);
   ISO8601d operator-  (const int);
   ISO8601d& operator+= (const int);
