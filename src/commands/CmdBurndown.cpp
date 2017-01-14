92af246f3af355831bdb15bcc00ba61156370bf8
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2017-01-14 00:16:43 +0000

    Add missing cast to double

--- src/commands/CmdBurndown.cpp
+++ src/commands/CmdBurndown.cpp
@@ -902,7 +902,7 @@ unsigned Chart::burndown_size (unsigned ntasks)
 
   // Choose the number from here rounded up to the nearest 10% of the next
   // highest power of 10 or half of power of 10.
-  const unsigned count = (unsigned) log10 (std::numeric_limits<unsigned>::max ());
+  const unsigned count = (unsigned) log10 (static_cast<double>(std::numeric_limits<unsigned>::max ()));
   unsigned half = 500;
   unsigned full = 1000;
 
