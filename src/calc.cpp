4da1785a1e264401a97c1f6bf8926d265dbdd2cb
Paul Beckingham <paul@beckingham.net>
2017-01-26 18:29:02 -0500

    Calc: Code cleanup

--- src/calc.cpp
+++ src/calc.cpp
@@ -114,7 +114,7 @@ int main (int argc, char** argv)
       e.evaluatePostfixExpression (expression, result);
 
     // Show any debug output.
-    for (auto& i : context.debugMessages)
+    for (const auto& i : context.debugMessages)
       std::cout << i << '\n';
 
     // Show the result in string form.
