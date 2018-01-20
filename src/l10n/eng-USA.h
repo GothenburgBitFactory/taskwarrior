a432cec4682a996d9553782527b92ce6b549573a
Paul Beckingham <paul@beckingham.net>
2018-01-20 13:52:41 -0500

    l10n: Eliminated STRING_TRIVIAL_INPUT

--- src/l10n/eng-USA.h
+++ src/l10n/eng-USA.h
@@ -482,7 +482,6 @@
 
 // Errors
 // TODO Move each of these to appropriate section.
-#define STRING_TRIVIAL_INPUT         "You must specify a command or a task to modify."
 #define STRING_INFINITE_LOOP         "Terminated substitution because more than {1} changes were made - infinite loop protection."
 #define STRING_UDA_TYPE              "User defined attributes may only be of type 'string', 'date', 'duration' or 'numeric'."
 #define STRING_UDA_COLLISION         "The UDA named '{1}' is the same as a core attribute, and is not permitted."
