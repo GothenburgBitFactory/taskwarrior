3c73af7964f2e638a3969bdb4d2d5085df922b28
Paul Beckingham <paul@beckingham.net>
2019-03-02 14:23:53 -0500

    Updated Copyright

--- src/Eval.cpp
+++ src/Eval.cpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 //
-// Copyright 2013 - 2018, Paul Beckingham, Federico Hernandez.
+// Copyright 2013 - 2019, Paul Beckingham, Federico Hernandez.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
