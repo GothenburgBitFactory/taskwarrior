f3175f94a29f7a2aaa9224c0ab5d54599e0a20b2
Rosen Penev <rosenp@gmail.com>
2019-09-27 19:20:23 -0700

    [clang-tidy] Use new range based loops
    Found with modernize-loop-convert
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/ViewTask.cpp
+++ src/ViewTask.cpp
@@ -320,8 +320,8 @@ std::string ViewTask::render (std::vector <Task>& data, std::vector <int>& seque
 
       if (obfuscate)
         if (_columns[c]->type () == "string")
-          for (unsigned int line = 0; line < cells[c].size (); ++line)
-            cells[c][line] = obfuscateText (cells[c][line]);
+          for (auto & line : cells[c])
+            line = obfuscateText (line);
     }
 
     // Listing breaks are simply blank lines inserted when a column value
