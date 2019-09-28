674aab8d9dcd7cee93dae5a0bc5f8f5465f1c067
Rosen Penev <rosenp@gmail.com>
2019-09-27 20:34:25 -0700

    [clang-tidy] Reserve memory for vector loop
    Found with performance-inefficient-vector-operation
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/feedback.cpp
+++ src/feedback.cpp
@@ -47,6 +47,7 @@ static void countTasks (const std::vector <Task>&, const std::string&, int&, int
 std::string taskIdentifiers (const std::vector <Task>& tasks)
 {
   std::vector <std::string> identifiers;
+  identifiers.reserve(tasks.size());
   for (const auto& task: tasks)
     identifiers.push_back (task.identifier (true));
 
