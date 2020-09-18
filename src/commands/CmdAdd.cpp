GothenburgBitFactory/taskwarrior

commit 4e80d5d5fdb317f746a902ba83c891d7b20fe44f

[TW-1938] Resolve conflicts between new-id and new-uuid
- new-uuid is *not* set by default
- new-uuid overrides new-id when set

The reasoning is that new-uuid is mostly used by code (eg: Python's
taskw module), whereas new-id is mostly used by humans.

By making new-uuid not set by default, calling code can set it to
receive UUIDs (preserving 2.5.x behaviour), but humans can continue to
use integer task IDs.

src/commands/CmdAdd.cpp
@@ -60,7 +60,21 @@ int CmdAdd::execute (std::string& output)
 
   // Do not display ID 0, users cannot query by that
   auto status = task.getStatus ();
-  if (context.verbose ("new-id") &&
+
+  // We may have a situation where both new-id and new-uuid config
+  // variables are set. In that case, we'll show the new-uuid, as
+  // it's enduring and never changes, and it's unlikely the caller
+  // asked for this if they just wanted a human-friendly number.
+
+  if (context.verbose ("new-uuid") &&
+           status != Task::recurring)
+    output += format (STRING_CMD_ADD_FEEDBACK, task.get ("uuid")) + '\n';
+
+  else if (context.verbose ("new-uuid") &&
+           status == Task::recurring)
+    output += format (STRING_CMD_ADD_RECUR, task.get ("uuid")) + '\n';
+
+  else if (context.verbose ("new-id") &&
       (status == Task::pending ||
        status == Task::waiting))
     output += format (STRING_CMD_ADD_FEEDBACK, task.id) + '\n';
@@ -68,14 +82,6 @@ int CmdAdd::execute (std::string& output)
   else if (context.verbose ("new-id") &&
            status == Task::recurring)
     output += format (STRING_CMD_ADD_RECUR, task.id) + '\n';
-
-  else if (context.verbose ("new-uuid") &&
-           status != Task::recurring)
-    output += format (STRING_CMD_ADD_FEEDBACK, task.get ("uuid")) + '\n';
-
-  else if (context.verbose ("new-uuid") &&
-           status == Task::recurring)
-    output += format (STRING_CMD_ADD_RECUR, task.get ("uuid")) + '\n';
 
   if (context.verbose ("project"))
     context.footnote (onProjectChange (task));

src/Context.cpp
@@ -88,7 +88,7 @@ std::string configurationDefaults =
   "\n"
   "# Miscellaneous\n"
   "#                                              # Comma-separated list.  May contain any subset of:\n"
-  "verbose=blank,header,footnote,label,new-id,new-uuid,affected,edit,special,project,sync,unwait,override,recur\n"
+  "verbose=blank,header,footnote,label,new-id,affected,edit,special,project,sync,unwait,override,recur\n"
   "confirmation=1                                 # Confirmation on delete, big changes\n"
   "recurrence=1                                   # Enable recurrence\n"
   "recurrence.confirmation=prompt                 # Confirmation for propagating changes among recurring tasks (yes/no/prompt)\n"

commit 10b64adb58988ec8d67891601ef394651fbc7bef

CmdAdd: Make sure a UUID is printed if ID does not exist
This fixes a regression was introduced in
4e80d5d5fdb317f746a902ba83c891d7b20fe44f.

Currently, if a user (with default configuration) adds a completed or
deleted task, they do not get a confirmation message ("Created task
..."). This is due to the fact that:
a) new-uuid is not set by default
b) new-id is set by default, but only prints message if the added task
   is in pending or waiting state

Make sure to print a message containing UUID reference if the newly
added task is in completed or deleted state.

src/commands/CmdAdd.cpp
@@ -63,12 +63,14 @@ int CmdAdd::execute (std::string& output)
   // asked for this if they just wanted a human-friendly number.
 
   if (Context::getContext ().verbose ("new-uuid") &&
-           status != Task::recurring)
-    output += format ("Created task {1}.\n", task.get ("uuid"));
-
-  else if (Context::getContext ().verbose ("new-uuid") &&
            status == Task::recurring)
     output += format ("Created task {1} (recurrence template).\n", task.get ("uuid"));
+
+  else if (Context::getContext ().verbose ("new-uuid") ||
+          (Context::getContext ().verbose ("new-id") &&
+            (status == Task::completed ||
+             status == Task::deleted)))
+    output += format ("Created task {1}.\n", task.get ("uuid"));
 
   else if (Context::getContext ().verbose ("new-id") &&
       (status == Task::pending ||