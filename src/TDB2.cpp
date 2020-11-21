GothenburgBitFactory/taskwarrior

commit 48dff115c2d237e0c94b97a868d81360358c460e

hook onModify into task undo (Closes: #2213)

src/TDB2.cpp
@@ -839,6 +839,9 @@ void TDB2::revert ()
     //   - erase from completed
     //   - if in backlog, erase, else cannot undo
 
+    Task old = Task (prior);
+    Context::getContext ().hooks.onModify (Task (current), old);
+
     // Modify other data files accordingly.
     std::vector <std::string> p = pending.get_lines ();
     revert_pending (p, uuid, prior);

commit 474828f8dc6c95ba5bc61a05e10cb1926f96c6e8

Fix Undo command
The Undo command broke in 48dff11 when the `onModify` hook got added to
its execution in order to fix #2213. The reason for it begin broke is
that a Task object is created in an unsafe manner because the `prior`
task may be empty.
Catching this case fixes the bug.

src/TDB2.cpp
@@ -839,7 +839,9 @@ void TDB2::revert ()
     //   - erase from completed
     //   - if in backlog, erase, else cannot undo
 
-    Task old = Task (prior);
+    Task old = Task ();
+    if (prior != "")
+      old = Task (prior);
     Context::getContext ().hooks.onModify (Task (current), old);
 
     // Modify other data files accordingly.