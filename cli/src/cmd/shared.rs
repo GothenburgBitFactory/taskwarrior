8f7e2e279005aa953006a72c3ce2ab88d934a590
Dustin J. Mitchell <dustin@mozilla.com>
2020-11-25 18:06:56 -0500

    add a 'task sync' command using a copy of the test server

--- cli/src/cmd/shared.rs
+++ cli/src/cmd/shared.rs
@@ -1,6 +1,7 @@
 use clap::Arg;
 use failure::{format_err, Fallible};
-use taskchampion::{Replica, Task, Uuid};
+use std::path::Path;
+use taskchampion::{server, taskstorage, Replica, Task, Uuid};
 
 pub(super) fn task_arg<'a>() -> Arg<'a, 'a> {
     Arg::with_name("task")
@@ -25,4 +26,32 @@ pub(super) fn get_task<S: AsRef<str>>(replica: &mut Replica, task_arg: S) -> Fal
     }
 
     Err(format_err!("Cannot interpret {:?} as a task", task_arg))
+}
+
+/// A command invocation contains all of the necessary regarding a single invocation of the CLI.
+#[derive(Debug)]
+pub struct CommandInvocation {
+    pub(crate) subcommand: Box<dyn super::SubCommandInvocation>,
+}
+
+impl CommandInvocation {
+    pub(crate) fn new(subcommand: Box<dyn super::SubCommandInvocation>) -> Self {
+        Self { subcommand }
+    }
+
+    pub fn run(self) -> Fallible<()> {
+        self.subcommand.run(&self)
+    }
+
+    // -- utilities for command invocations
+
+    pub(super) fn get_replica(&self) -> Replica {
+        Replica::new(Box::new(
+            taskstorage::KVStorage::new(Path::new("/tmp/tasks")).unwrap(),
+        ))
+    }
+
+    pub(super) fn get_server(&self) -> impl server::Server {
+        server::LocalServer::new()
+    }
 }
---
+++ taskchampion/src/server/local.rs
@@ -0,0 +1,79 @@
+use crate::server::{
+    AddVersionResult, GetVersionResult, HistorySegment, Server, VersionId, NO_VERSION_ID,
+};
+use failure::Fallible;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+struct Version {
+    version_id: VersionId,
+    parent_version_id: VersionId,
+    history_segment: HistorySegment,
+}
+
+pub struct LocalServer {
+    latest_version_id: VersionId,
+    // NOTE: indexed by parent_version_id!
+    versions: HashMap<VersionId, Version>,
+}
+
+impl LocalServer {
+    /// A test server has no notion of clients, signatures, encryption, etc.
+    pub fn new() -> LocalServer {
+        LocalServer {
+            latest_version_id: NO_VERSION_ID,
+            versions: HashMap::new(),
+        }
+    }
+}
+
+impl Server for LocalServer {
+    /// Add a new version.  If the given version number is incorrect, this responds with the
+    /// appropriate version and expects the caller to try again.
+    fn add_version(
+        &mut self,
+        parent_version_id: VersionId,
+        history_segment: HistorySegment,
+    ) -> Fallible<AddVersionResult> {
+        // no client lookup
+        // no signature validation
+
+        // check the parent_version_id for linearity
+        if self.latest_version_id != NO_VERSION_ID {
+            if parent_version_id != self.latest_version_id {
+                return Ok(AddVersionResult::ExpectedParentVersion(
+                    self.latest_version_id,
+                ));
+            }
+        }
+
+        // invent a new ID for this version
+        let version_id = Uuid::new_v4();
+
+        self.versions.insert(
+            parent_version_id,
+            Version {
+                version_id,
+                parent_version_id,
+                history_segment,
+            },
+        );
+        self.latest_version_id = version_id;
+
+        Ok(AddVersionResult::Ok(version_id))
+    }
+
+    /// Get a vector of all versions after `since_version`
+    fn get_child_version(&self, parent_version_id: VersionId) -> Fallible<GetVersionResult> {
+        if let Some(version) = self.versions.get(&parent_version_id) {
+            Ok(GetVersionResult::Version {
+                version_id: version.version_id,
+                parent_version_id: version.parent_version_id,
+                history_segment: version.history_segment.clone(),
+            })
+        } else {
+            Ok(GetVersionResult::NoSuchVersion)
+        }
+    }
+}
+
