cf078e123390dc02481b83993ea1b83eb2f9e4d9
Dustin J. Mitchell <dustin@mozilla.com>
2021-05-23 18:23:37 -0400

    add 'wait' to the info output

--- cli/src/invocation/cmd/info.rs
+++ cli/src/invocation/cmd/info.rs
@@ -36,6 +36,9 @@ pub(crate) fn execute<W: WriteColor>(
                 tags.sort();
                 t.add_row(row![b->"Tags", tags.join(" ")]);
             }
+            if task.is_waiting() {
+                t.add_row(row![b->"Wait", task.get_wait().unwrap()]);
+            }
         }
         t.print(w)?;
     }
