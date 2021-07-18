69b34dcef3379e70e64d584bc15f920c17acf39f
sebu06 <github@prebtec.de>
2021-07-18 20:26:24 +0200

    using convenience function for paying attention to verbosity tokens

--- src/commands/CmdDelete.cpp
+++ src/commands/CmdDelete.cpp
@@ -72,8 +72,8 @@ int CmdDelete::execute (std::string&)
   // Accumulated project change notifications.
   std::map <std::string, std::string> projectChanges;
 
-  if(filtered.size() > 1) {
-    std::cout << "This command will alter " << format(filtered.size()) << " tasks." << std::endl;
+  if(filtered.size() > 1) { 
+    feedback_affected("This command will alter {1} tasks.", filtered.size());
   }
   for (auto& task : filtered)
   {
--- src/commands/CmdStop.cpp
+++ src/commands/CmdStop.cpp
@@ -68,7 +68,7 @@ int CmdStop::execute (std::string&)
   std::map <std::string, std::string> projectChanges;
 
   if(filtered.size() > 1) {
-    std::cout << "This command will alter " << format(filtered.size()) << " tasks." << std::endl;
+    feedback_affected("This command will alter {1} tasks.", filtered.size());
   }
   for (auto& task : filtered)
   {
