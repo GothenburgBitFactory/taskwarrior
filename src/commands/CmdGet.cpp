e6ca9e58ef0aaf0d4a661ad63739a2967b46498a
Rosen Penev <rosenp@gmail.com>
2021-08-04 20:50:41 -0700

    use emplace
    Allows removing the constructor name as emplace forwards the arguments
    directly.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/commands/CmdGet.cpp
+++ src/commands/CmdGet.cpp
@@ -67,9 +67,9 @@ int CmdGet::execute (std::string& output)
         Task t;
         Variant result;
         if (getDOM (arg.attribute ("raw"), t, result))
-          results.push_back ((std::string) result);
+          results.emplace_back (result);
         else
-          results.push_back ("");
+          results.emplace_back ("");
       }
       break;
 
