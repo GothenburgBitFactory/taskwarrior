ddfb327292ddf4d716ddf932abf685d1c6a8c8c4
Dustin J. Mitchell <dustin@v.igoro.us>
2021-10-15 02:56:46 +0000

    WIP

--- taskchampion/src/server/remote/crypto.rs
+++ taskchampion/src/server/remote/crypto.rs
@@ -1,5 +1,6 @@
 use crate::server::HistorySegment;
-use std::io::Read;
+use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
+use std::io::{Cursor, Read};
 use tindercrypt::cryptors::RingCryptor;
 use uuid::Uuid;
 
@@ -14,6 +15,60 @@ impl From<Vec<u8>> for Secret {
 impl AsRef<[u8]> for Secret {
     fn as_ref(&self) -> &[u8] {
         &self.0
+    }
+}
+
+const PBKDF2_SALT_SIZE: usize = 32;
+const NONCE_SIZE: usize = 12;
+const ENVELOPE_VERSION: u32 = 1;
+
+/// Envelope for the data stored on the server, containing the information
+/// required to decrypt.
+#[derive(Debug, PartialEq, Eq)]
+struct Envelope {
+    kdf_iterations: u32,
+    kdf_salt: [u8; PBKDF2_SALT_SIZE],
+    nonce: [u8; NONCE_SIZE],
+}
+
+impl Envelope {
+    const SIZE: usize = 4 + 4 + PBKDF2_SALT_SIZE + NONCE_SIZE;
+
+    fn from_bytes(buf: &[u8]) -> anyhow::Result<Self> {
+        if buf.len() < 4 {
+            anyhow::bail!("envelope is too small");
+        }
+
+        let mut rdr = Cursor::new(buf);
+        let version = rdr.read_u32::<NetworkEndian>().unwrap();
+        if version != 1 {
+            anyhow::bail!("unrecognized envelope version {}", version);
+        }
+        if buf.len() != Envelope::SIZE {
+            anyhow::bail!("envelope size {} is not {}", buf.len(), Envelope::SIZE);
+        }
+
+        let kdf_iterations = rdr.read_u32::<NetworkEndian>().unwrap();
+
+        let mut env = Envelope {
+            kdf_iterations,
+            kdf_salt: [0; PBKDF2_SALT_SIZE],
+            nonce: [0; NONCE_SIZE],
+        };
+        env.kdf_salt.clone_from_slice(&buf[8..8 + PBKDF2_SALT_SIZE]);
+        env.nonce
+            .clone_from_slice(&buf[8 + PBKDF2_SALT_SIZE..8 + PBKDF2_SALT_SIZE + NONCE_SIZE]);
+        Ok(env) // TODO: test
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(Envelope::SIZE);
+
+        buf.write_u32::<NetworkEndian>(ENVELOPE_VERSION).unwrap();
+        buf.write_u32::<NetworkEndian>(self.kdf_iterations).unwrap();
+        buf.extend_from_slice(&self.kdf_salt);
+        buf.extend_from_slice(&self.nonce);
+        buf // TODO: test
     }
 }
 
@@ -74,6 +129,19 @@ impl AsRef<[u8]> for Ciphertext {
 mod test {
     use super::*;
     use pretty_assertions::assert_eq;
+
+    #[test]
+    fn envelope_round_trip() {
+        let env = Envelope {
+            kdf_iterations: 100,
+            kdf_salt: [1; 32],
+            nonce: [2; 12],
+        };
+
+        let bytes = env.to_bytes();
+        let env2 = Envelope::from_bytes(&bytes).unwrap();
+        assert_eq!(env, env2);
+    }
 
     #[test]
     fn round_trip() {
