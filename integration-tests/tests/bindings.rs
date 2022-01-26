96b59dd5b2bd99cb1fe05463ec17c46492e7c5e6
Dustin J. Mitchell <dustin@v.igoro.us>
2022-01-26 01:38:25 +0000

    serialize C integration tests

--- integration-tests/tests/bindings.rs
+++ integration-tests/tests/bindings.rs
@@ -1,7 +1,17 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+lazy_static! {
+    // the C library running the tests is not reentrant, so we use a mutex to ensure that only one
+    // test runs at a time.
+    static ref MUTEX: Mutex<()> = Mutex::new(());
+}
+
 macro_rules! suite(
     { $s:ident } => {
     #[test]
     fn $s() {
+        let _guard = MUTEX.lock().unwrap();
         assert_eq!(integration_tests::bindings_tests::$s(), 0);
     }
     };
