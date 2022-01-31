fd1e2a1cc489402805df0c7b043c8e179438ad7e
Sebastian Marquardt <sebastian.marquardt@markwaerter.de>
2022-01-31 01:17:01 +0100

    Build as part of container deployment without cluttering local copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -euo pipefail
+
+cp -r /root/src /root/code
+cd /root/code
 
 git submodule init
 git submodule update
+if [ -f CMakeCache.txt ]; then
+  rm CMakeCache.txt
+fi
 cmake -DCMAKE_BUILD_TYPE=debug .
 make -j2
 make install
@@ -11,7 +18,3 @@ cd test || exit
 make
 ./run_all -v
 ./problems
-make clean
-
-cd ../
-make clean
