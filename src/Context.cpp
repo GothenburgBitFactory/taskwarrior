0a558ef7c8f3321e7451294512c885661f5b4a84
Tomas Babej <tomas@tbabej.com>
2022-05-07 21:52:22 -0400

    Context: Add a reminder to mirror options in the vim syntax file

--- src/Context.cpp
+++ src/Context.cpp
@@ -58,6 +58,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // This string is parsed and used as default values for configuration.
+// Note: New configuration options should be added to the vim syntax file in scripts/vim/syntax/taskrc.vim
 std::string configurationDefaults =
   "# Taskwarrior program configuration file.\n"
   "# For more documentation, see https://taskwarrior.org or try 'man task', 'man task-color',\n"
