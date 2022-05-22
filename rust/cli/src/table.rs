c0ce1fe059137fd39cf61ada6c55356fee149f50
Dustin J. Mitchell <dustin@v.igoro.us>
2022-05-22 01:40:32 +0000

    Drop the 'taskchampion-cli' crate
    It was fun while it lasted, but we'll be using TaskWarrior for the CLI!

--- rust/cli/src/table.rs
+++
@@ -1,8 +0,0 @@
-use prettytable::format;
-
-pub(crate) fn format() -> format::TableFormat {
-    format::FormatBuilder::new()
-        .column_separator(' ')
-        .borders(' ')
-        .build()
-}
