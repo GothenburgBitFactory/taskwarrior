410efc548d38df9461f7aafc6b8f544f0d664641
Mauro Scomparin <scompo@gmail.com>
2022-11-25 10:41:02 +0100

    Scheduled attribute updated in handleRecurrence.
    The "scheduled" attribute of tasks generated by recurring tasks is now
    updated the same way the "wait" attribute is handled.
    
    The difference between the original "scheduled" and "due" dates is
    added to the "due" date of each generated task when a "scheduled" date is
    present.

--- src/recur.cpp
+++ src/recur.cpp
@@ -118,6 +118,13 @@ void handleRecurrence() {
             rec.setStatus(Task::pending);
           }
 
+          if (t.has("scheduled")) {
+            Datetime old_scheduled(t.get_date("scheduled"));
+            Datetime old_due(t.get_date("due"));
+            Datetime due(d);
+            rec.set("scheduled", format((due + (old_scheduled - old_due)).toEpoch()));
+          }
+
           rec.set("imask", i);
           rec.remove("mask");  // Remove the mask of the parent.
 
