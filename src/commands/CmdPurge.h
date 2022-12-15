fb8ca8a264b7f4b4a419cb74ec09dd338074c23a
Dustin J. Mitchell <djmitche@google.com>
2022-12-15 02:52:45 +0000

    [BREAKING] remove 'task purge' support
    TaskChampion removes deleted tasks automatically when they expire, so
    this subcommand is no longer required.

--- src/commands/CmdPurge.h
+++ src/commands/CmdPurge.h
@@ -32,10 +32,6 @@
 
 class CmdPurge : public Command
 {
-private:
-  void purgeTask (Task& task, int& count);
-  void handleChildren (Task& task, int& count);
-  void handleDeps (Task& task);
 public:
   CmdPurge ();
   int execute (std::string&);
