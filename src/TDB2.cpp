e7ed5492d8b35f2440da66e54956c3e095351400
Dustin J. Mitchell <djmitche@google.com>
2024-03-31 16:13:41 -0400

    review suggestions

--- src/TDB2.cpp
+++ src/TDB2.cpp
@@ -62,9 +62,8 @@ void TDB2::open_replica (const std::string& location, bool create_if_missing)
     Color warning = Color (Context::getContext ().config.get ("color.warning"));
     std::cout << warning.colorize (
       format ("Found existing '.data' files in {1}", location)) << "\n";
-    std::cout << "  Taskwarrior's storage format changed in 3.0.\n";
+    std::cout << "  Taskwarrior's storage format changed in 3.0, requiring a manual migration.\n";
     std::cout << "  See https://github.com/GothenburgBitFactory/taskwarrior/releases.\n";
-    std::cout << "  Remove the files to silence this warning.\n\n";
   }
   replica = tc::Replica(location, create_if_missing);
 }
