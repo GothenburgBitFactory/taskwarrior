847c482c250345e3264dc88fd1fec1d186344b4b
koleesch <st.kornemann@posteo.de>
2024-07-07 19:19:54 +0200

    Remove duplicate check from task diag (#3545)

--- test/uuid.test.py
+++ test/uuid.test.py
@@ -174,42 +174,6 @@ class TestUUID(TestCase):
         self.assertIn('"description":"seven"', out)
 
 
-class TestUUIDuplicates(TestCase):
-    def setUp(self):
-        """Executed before each test in the class"""
-        self.t = Task()
-
-    def test_uuid_duplicates_dupe(self):
-        """Verify that duplicating tasks does not create duplicate UUIDs"""
-        self.t("add simple")
-        self.t("1 duplicate")
-
-        uuids = list()
-        for id in range(1,3):
-            code, out, err = self.t("_get %d.uuid" % id)
-            uuids.append(out.strip())
-
-        self.assertEqual(len(uuids), len(set(uuids)))
-
-        code, out, err = self.t("diag")
-        self.assertIn("No duplicates found", out)
-
-    def test_uuid_duplicates_recurrence(self):
-        """Verify that recurring tasks do not create duplicate UUIDs"""
-        print(self.t("add periodic recur:daily due:yesterday"))
-        self.t("list") # GC/handleRecurrence
-
-        uuids = list()
-        for id in range(1,5):
-            code, out, err = self.t("_get %d.uuid" % id)
-            uuids.append(out.strip())
-
-        self.assertEqual(len(uuids), len(set(uuids)))
-
-        code, out, err = self.t("diag")
-        self.assertIn("No duplicates found", out)
-
-
 class TestBug954(TestCase):
     def setUp(self):
         """Executed before each test in the class"""
