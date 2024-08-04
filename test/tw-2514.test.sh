36aa58d99ff399584691f58d5a6ac2b84e5bc310
Dustin J. Mitchell <djmitche@google.com>
2024-08-04 18:12:03 -0400

    Actually run shell tests
    Two of these used EXPFAIL which, because nothing is interpreting the TAP
    output, does not work. So, that functionality is removed, and the
    expected-to-fail bits are commented out or removed.
    
    There was a conditional on the filename in `bash_tap.sh` which caused it
    to not actually do anything and just run the test as a simple shell
    script. That, too, has been removed.

--- test/tw-2514.test.sh
+++ test/tw-2514.test.sh
@@ -7,8 +7,6 @@ task 1 mod start:yesterday+18h
 task 1 done end:yesterday+20h
 
 # this does not work without journal.info
-export EXPFAIL=true
-
 # Check that 2 hour interval is reported by task info
-task info | grep -F "Start deleted"
-[[ ! -z `task info | grep -F "Start deleted (duration: 2:00:00)."` ]]
+#task info | grep -F "Start deleted"
+#[[ ! -z `task info | grep -F "Start deleted (duration: 2:00:00)."` ]]
