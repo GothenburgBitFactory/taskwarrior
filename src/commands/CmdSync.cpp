e12f1ba1f71d29be07692e1bd27bb8e3d754859d
Dustin J. Mitchell <dustin@v.igoro.us>
2024-12-17 00:54:23 +0000

    more concise way of checking AWS config

--- src/commands/CmdSync.cpp
+++ src/commands/CmdSync.cpp
@@ -100,30 +100,21 @@ int CmdSync::execute(std::string& output) {
       throw std::string("sync.encryption_secret is required");
     }
 
-    int num_cred_methods = 0;
     bool using_profile = false;
     bool using_creds = false;
     bool using_default = false;
     if (aws_profile != "") {
       using_profile = true;
-      num_cred_methods++;
     }
     if (aws_access_key_id != "" || aws_secret_access_key != "") {
       using_creds = true;
-      num_cred_methods++;
     }
     if (aws_default_credentials != "") {
       using_default = true;
-      num_cred_methods++;
     }
 
-    if (!using_profile && !using_creds && !using_default) {
-      throw std::string("configuration for AWS credentials is required");
-    }
-
-    std::cout << "num_cred_method " << num_cred_methods << "\n";
-    if (num_cred_methods > 1) {
-      throw std::string("only one method of specifying AWS credentials is allowed");
+    if (using_profile + using_creds + using_default != 1) {
+      throw std::string("exactly one method of specifying AWS credentials is required");
     }
 
     if (verbose) {
